dc50e45a7da78d51f5f15dd236f19981684ec297
satyamsingh888 <888hellboysatyam123@gmail.com>
2024-01-06 18:51:44 +0530

    learning

---
+++ 01_basic/01_vatiables.js
@@ -0,0 +1,20 @@
+const accountID =34445
+let accountEmail = "singh@gami.com"
+var accountPassword = "345"
+accountCity ="MaharastrA"
+
+// accountId =2  //not allowed
+
+accountEmail ="hc@ng.com"
+accountPassword= "34344"
+accountCity ="Goa"
+let accountState;
+console.log(accountID);
+
+/*
+Prefer not use var
+because of issue in block scope and functional scope
+*/
+
+console.table([accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basic/02_DataTypes.js
@@ -0,0 +1,22 @@
+"use strict"; //treat all JS code as newer version
+
+// alert(3+3) // WE are using nodejs, not browser
+
+let name = "singh"
+let age = 34
+let isLoggedIn =false
+
+
+//number => 2 to the power 53
+//bignit
+//string => ""
+// boolean => true/false
+//null => standalone value
+//undefined =>
+//symbol 
+
+//object
+
+console.log(typeof"singh")
+console.log(typeof null) // object
+console.log(typeof undefined) // undefined
