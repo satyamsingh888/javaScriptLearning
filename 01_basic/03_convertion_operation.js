f3a51014d6482275c2f8b7a2f237fd3c7adbfed4
satyamsingh888 <888hellboysatyam123@gmail.com>
2024-01-07 00:26:29 +0530

    conversion of datatype

---
+++ 01_basic/03_convertion_operation.js
@@ -0,0 +1,27 @@
+// let score ="45"
+
+// console.log(typeof score);
+// console.log(typeof(score) );
+
+// let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber) );
+
+
+//"33" ==> 33
+//"33abc" ==> NaN
+// true ==>1; false ==> 0
+
+let isLoggedIn =""
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof(booleanIsLoggedIn));
+
+//1 =. true, 0 => false
+//"" => false
+//"sing" =>true
+
+let someNumber =33
+let stringNumber =String(someNumber)
+console.log(someNumber);
+console.log(typeof stringNumber)
