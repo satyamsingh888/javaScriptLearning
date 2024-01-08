0cb4f70b7fc723b16bca0655b31848a895c60760
satyamsingh888 <888hellboysatyam123@gmail.com>
2024-01-08 22:17:51 +0530

    leaningOneStepMore

---
+++ 01_basic/datatypes_summary.js
@@ -0,0 +1,57 @@
+// # Primitive 
+
+//7 types : string, Number , Boolean , null , undefined, 
+    // symbol, BigInt
+
+const score = 100
+const scoreValue = 100.45
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail; 
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 234232534534453n
+
+
+// Reference (Non primitive)    
+
+// Array, Objects , Functions
+
+
+const heros = ["Hulk" , "Ironman", "Thor"]
+{
+    name: "satyam"
+    age : 34
+
+}
+
+const myfunction= function(){
+console.log("hello World");
+}
+
+console.log(typeof scoreValue);
+
+
+
+/*        Typeof
+
+
+string  typeof   string
+Boolean  typeof   boolean
+Number   typeof   number
+Undefined  typeof   undefined
+Symbol     typeof   symbol
+function   typeof    object
+Null       typeof     object
+float      typeof      Number
+
+*/
+
+
+
+
