ccd3feecbc77b6cd2b42ea014727ade754a02cf2
satyamsingh888 <888hellboysatyam123@gmail.com>
2024-01-14 12:55:49 +0530

    learning maths and num

--- 01_basic/05_string.js
+++ 01_basic/05_string.js
@@ -1,49 +1,49 @@
-// const name = "singh"
-// const repoCount = 50
+// // const name = "singh"
+// // const repoCount = 50
 
-// // console.log(name + repoCount + " Value");
+// // // console.log(name + repoCount + " Value");
 
-// console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
+// // console.log(`hello my name is ${name} and my repoCount is ${repoCount}`);
 
-const name = "Satyam"
-let wshing = "Good Morning"
+// const name = "Satyam"
+// let wshing = "Good Morning"
 
-console.log(`${wshing} ${name}`);
-
-
-const gameName = new String('Satyamsingh-singh-bling')
-
-console.log(gameName[0]);
-console.log(gameName.__proto__);
+// console.log(`${wshing} ${name}`);
 
 
-console.log(gameName.length);
-console.log(gameName.toUpperCase());
-console.log(gameName.toLowerCase());
-console.log(gameName.charAt(4));
-console.log(gameName.indexOf('t'));
+// const gameName = new String('Satyamsingh-singh-bling')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
 
 
-
-const newString = gameName.substring(0, 7)
-console.log(newString);
-
-const anotherString = gameName.slice(-9 , 6)
-console.log(anotherString);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(4));
+// console.log(gameName.indexOf('t'));
 
 
 
-const newStringOne = "     hitesh    "
-console.log(newStringOne);
-console.log(newStringOne.trim());
+// const newString = gameName.substring(0, 7)
+// console.log(newString);
 
-const url = "https://hitesh.comhitesh%20chaudhary"
+// const anotherString = gameName.slice(-9 , 6)
+// console.log(anotherString);
 
-console.log(url.replace('%20','_'));
 
-console.log(url.includes('mona'))
 
-console.log(gameName.split('-'));
+// const newStringOne = "     hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+// const url = "https://hitesh.comhitesh%20chaudhary"
+
+// console.log(url.replace('%20','_'));
+
+// console.log(url.includes('mona'))
+
+// console.log(gameName.split('-'));
 
 
 
---
+++ 01_basic/06_num_and_maths.js
@@ -0,0 +1,41 @@
+const score= 400
+// console.log(score);
+
+const balance = new Number(233)
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 34.3434
+
+// console.log(otherNumber.toPrecision(2));
+
+const hunderds = 20000000
+// console.log(hunderds.toLocaleString('en-IN'));
+
+//**********************Maths************************* */
+
+// console.log(Math);
+// console.log(Math.abs(-5));
+// console.log(Math.round(5.78));
+// console.log(Math.ceil(5.78));
+// console.log(Math.floor(5.78));
+// console.log(Math.min(2,6,45,89,));
+// console.log(Math.max(2,6,45,89,));
+
+
+console.log(Math.random());
+console.log(Math.random()*10) +1 ;
+console.log(Math.floor(Math.random()*10)+1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random ()* (max - min + 1)) + min);
+
+
+
+
+
+
