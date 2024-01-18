466d27702278ca5a6ad4459de812b61f9c72d53a
satyamsingh888 <888hellboysatyam123@gmail.com>
2024-01-18 20:07:45 +0530

    take all code from chai or code

--- 02_basic/02_Arrays.js
+++ 02_basic/02_Arrays.js
@@ -21,9 +21,14 @@ console.log(Array.from("singh"))
 
 console.log(Array.from({name:"singh"}));
 
-let score1 = 100
-let score2 = 200
-let score3 = 300
-console.log(Array.of(score1,score2,score3));
+// let score1 = 100
+// let score2 = 200
+// let score3 = 300
+// console.log(Array.of(score1,score2,score3));
+
+let name = "Ram"
+let anotherName = "Shyam"
+let ThirdName = "Rohan"
+console.log(Array.of(name,anotherName,ThirdName));
 
 
