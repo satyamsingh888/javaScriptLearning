1e06b3f4b219e25e3ba237913c91ec06e636cfac
vboxuser <vboxuser@ubuntu.myguest.virtualbox.org>
2024-03-03 01:19:03 +0530

    learning

---
+++ Jonas Sir/01_Fundamentals-part-1/Logical_Operators.js
@@ -0,0 +1,30 @@
+const hasDriversLinces = true; //A
+const hasGoodVision = true; //B
+
+console.log(hasDriversLinces && hasGoodVision);
+console.log(hasDriversLinces || hasGoodVision);
+console.log( ! hasGoodVision);
+
+// const shouldDrive = hasDriversLinces && hasGoodVision 
+
+// if(shouldDrive)
+// {
+//     console.log("sara is able to drive");
+
+// }else{
+//     console.log("Someone else should drive");
+// }
+
+const isTired = false; // C
+console.log(hasDriversLinces && hasGoodVision || isTired);
+
+
+const shouldDrive = hasDriversLinces && hasGoodVision && isTired
+if(shouldDrive)
+{
+    console.log("sara is able to drive");
+
+}else{
+    console.log("Someone else should drive");
+}
+
