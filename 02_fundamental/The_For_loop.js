c790cdf3dac8557e09970c2e06dc0f533d028fbc
Satyam Singh <888hellboysatyam123@gmail.com>
2025-01-27 17:16:49 +0530

    learning

--- 02_fundamental/The_For_loop.js
+++ 02_fundamental/The_For_loop.js
@@ -23,10 +23,46 @@ const jonasArray = [
 ];
 const types = [];
 
-for(let i =0; i< jonasArray.length; i++){
-    console.log(jonasArray[i], ); 
-    // Filling types array
-    // types[i] = typeof jonasArray[i];
-    types.push(typeof jonasArray[i]);
+for (let i = 0; i < jonasArray.length; i++) {
+  //Reading from jonas Array
+  console.log(jonasArray[i], typeof jonasArray[i]);
+  // filling types array
+  // types[i] = typeof jonasArray[i];
+  types.push(typeof jonasArray[i]);
 }
 console.log(types);
+
+const years = [2001, 2002, 2003, 2004, 2005];
+const ages = [];
+for (let i = 0; i < years.length; i++) {
+  ages.push(2024 - years[i]);
+  // ages[i]= 2024- years[i]
+}
+console.log(ages);
+
+
+// continue and break
+console.log('---ONLY STRINGS---');
+for(let i =0; i< jonasArray.length; i++){
+  if(typeof jonasArray[i] !== 'string') continue;
+  console.log(jonasArray[i] , typeof jonasArray[i]);
+}
+for(let i =0; i< jonasArray.length; i++){
+  if(typeof jonasArray[i] !== 'string') break;
+  console.log(jonasArray[i] , typeof jonasArray[i]);
+};
+
+
+for(let i = jonasArray.length -1; i>=0; i--){
+  console.log(i,jonasArray[i]);
+}
+
+
+for(let exercise  =1; exercise<4; exercise++){
+console.log(`-----Starting exercise ${exercise}`);
+
+
+for(let rep = 1; rep<6; rep++){
+  console.log(`Exercise Lifting weight repetition ${rep}`);
+}
+};
